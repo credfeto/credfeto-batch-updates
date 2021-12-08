dbfc05c6365c0a48a30d3bd609ff10a09a376d9d
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-12-08 00:33:12 +0000

    Updated scripts

--- update-funfair.sh
+++ update-funfair.sh
@@ -1,6 +1,53 @@
 #! /bin/bash
 
+PROG=$0
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+function error_exit {
+    echo
+    echo "$@"
+    exit 1
+}
+#Trap the killer signals so that we can exit with a good message.
+trap "error_exit 'Received signal SIGHUP'" SIGHUP
+trap "error_exit 'Received signal SIGINT'" SIGINT
+trap "error_exit 'Received signal SIGTERM'" SIGTERM
+
+shopt -s expand_aliases
+alias die='error_exit "Error $PROG (@`echo $(( $LINENO - 1 ))`):"'
+
+REPOS=
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -r|--repositories)
+      REPOS="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)    # unknown option
+      die "unknown argument $1"
+      ;;
+  esac
+done
+
+[ -z "$REPOS" ] && die "--repositories not specified"
+
+WORK=${SCRIPT_DIR}/../work/${REPOS}/
+LOGS=${SCRIPT_DIR}/../logs/${REPOS}/
+REPOS_LIST=${SCRIPT_DIR}/${REPOS}/repos.lst
+
+echo "Work: ${WORK}"
+echo "Logs: ${LOGS}"
+echo "Repos.lst : ${REPOS_LIST}"
+
+die "Not ready"
+
 echo $PATH
+
+mkdir 
 
 cd /home/templateupdate/auto-update/template-update
 pwd
